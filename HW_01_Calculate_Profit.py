559c50bb91537626885357653e30bb8d5b0944a3
barış şahin <99876715+brs14shn@users.noreply.github.com>
2022-03-26 18:03:56 +0300

    Update HW_01_Calculate_Profit.py

--- HW_01_Calculate_Profit.py
+++ HW_01_Calculate_Profit.py
@@ -1,7 +1,7 @@
 
 
-**If you had deposited a coin on the cryptocurrency exchange
- that brought 7% fixed profit daily for a week, how much would your $ 1000 reach at the end of the 7th day?**
+*If you had deposited a coin on the cryptocurrency exchange
+ that brought 7% fixed profit daily for a week, how much would your $ 1000 reach at the end of the 7th day?*
 
  
 
